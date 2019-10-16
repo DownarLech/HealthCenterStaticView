941f5e8ab6059d4e6f3b6a93f48f7d1b306f9216
Your Name <you@example.com>
2019-10-16 21:51:03 +0200

    Added all files

--- README.md
+++
@@ -1,1 +0,0 @@
-Static views for the web application supporting registration in health center. Login, sign up, select your appointment, see your appointments.
---
+++ php/script.php
@@ -0,0 +1,18 @@
+$("#basicDate").flatpickr({
+    enableTime: true,
+    dateFormat: "F, d Y H:i",
+	time_24hr: true,
+	minuteIncrement: 30,
+	minTime: "8:00",
+    maxTime: "18:00",
+	disableMobile: "true",
+	"locale": "pl"
+
+});
+
+
+
+	
+
+
+
